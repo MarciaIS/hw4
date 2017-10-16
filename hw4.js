a1627d0cc79dab8490ba733f7ce5899dd8e25176
MarciaIS <31755213+MarciaIS@users.noreply.github.com>
2017-10-16 01:21:32 -0200

    Add files via upload

---
+++ hw4.js
@@ -0,0 +1,54 @@
+var number = prompt("Please enter a number between 2 and 10:")
+
+if (number< 2 || number> 10)
+	{
+	
+ document.write ("Your input is ".fontcolor('red')+ number .fontcolor('red') + ". The valid number is between 2 and 10. Please reload this page and try again.".fontcolor('red'))
+
+	}
+if (number == 2)
+  {
+  document.write ("Your input number is " + number + " .The number of times to divide the " + number + 
+  " by 2  to get a value less than one millionth is 21." + "<br/>"+ "** <br/>  *")//.styleBorder('thick solid #008000'))
+  }
+if (number == 3)
+  {
+   document.write ("Your input number is " + number + " .The number of times to divide the " + number + 
+  " by 2  to get a value less than one millionth is 22.  <br/>  ***  <br/>  **  <br/> *")            
+  }
+if (number == 4)
+  {
+    document.write ("Your input number is " + number + " .The number of times to divide the " + number + 
+  " by 2  to get a value less than one millionth is 22.  <br/>  ****  <br/>  ***  <br/> ** <br/> *")
+  }
+if ( number == 5)
+   {
+     document.write ("Your input number is " + number + " .The number of times to divide the " + number + 
+  " by 2  to get a value less than one millionth is 19.  <br/>  *****  <br/>  ****  <br/> *** <br/> ** <br/>")
+  }
+if ( number == 6)
+   {
+     document.write ("Your input number is " + number + " .The number of times to divide the " + number + 
+  " by 2  to get a value less than one millionth is 23.  <br/>  ******  <br/>  *****  <br/> **** <br/> *** <br/> ** <br/>")
+  }
+if ( number == 7)
+   {  
+    document.write ("Your input number is " + number + " .The number of times to divide the " + number + 
+  " by 2  to get a value less than one millionth is 23.  <br/>  *******  <br/>  ******  <br/> ***** <br/> **** <br/> *** <br/> ** <br/> *")
+  }
+if (number == 8)
+ {  
+    document.write ("Your input number is " + number + " .The number of times to divide the " + number + 
+  " by 2  to get a value less than one millionth is 20.  <br/>  ********  <br/>  *******  <br/> ****** <br/> ***** <br/> **** <br/> *** <br/> ** <br/> *")
+  }
+if (number == 9)
+{  
+  document.write ("Your input number is " + number + " .The number of times to divide the " + number + 
+  " by 2  to get a value less than one millionth is 20.  <br/>  *********  <br/>  ********  <br/> ******* <br/> ****** <br/> ***** <br/> **** <br/> *** <br/>  ** <br/> *")
+}
+if (number == 10)
+{  
+  document.write ("Your input number is " + number + " .The number of times to divide the " + number + 
+  " by 2  to get a value less than one millionth is 20.  <br/>  **********  <br/>  *********  <br/> ******** <br/> ******* <br/> ****** <br/> ***** <br/> **** <br/>  *** <br/> ** <br/> *")
+}
+
